fdb5b804c0188d9738ba82663b023f632972b363
Timóteo <timoteo@consorciocred.com>
2023-04-04 10:10:47 -0300

    feat: add app header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import { globalStyles } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app';
 import type { AppProps } from 'next/app'
+
+import logoImg from '../assets/logo.svg'
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
