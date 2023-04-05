af942db1034e3671181fce2705e7186a1f208221
Timóteo <timoteostifft@gmail.com>
2023-04-05 11:55:25 -0300

    feat: add next image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
-import { globalStyles } from '@/styles/global'
-import { Container, Header } from '@/styles/pages/app';
 import type { AppProps } from 'next/app'
+import { globalStyles } from '@/styles/global'
 
+import { Container, Header } from '@/styles/pages/app';
+
+import Image from 'next/image';
 import logoImg from '../assets/logo.svg'
 
 globalStyles();
@@ -10,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt=""/>
       </Header>
 
       <Component {...pageProps} />
