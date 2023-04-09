e94373472770ff9a61223894e671a3c1579b7afc
Timóteo <timoteostifft@gmail.com>
2023-04-08 21:22:21 -0300

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react'
 
+import Image from 'next/image';
+import { HomeContainer, Product } from '@/styles/pages/home'
+
+import shirt1 from '../assets/shirts/1.png';
+import shirt2 from '../assets/shirts/2.png';
+// import shirt3 from '../assets/shirts/3.png';
+
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
-  )
+      <HomeContainer>
+        <Product>
+          <Image src={shirt1} width={520} height={480} alt=""/>
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+
+        <Product>
+          <Image src={shirt2} width={520} height={480} alt=""/>
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+
+        
+      </HomeContainer>
+    )
 }
