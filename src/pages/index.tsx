ca04f39c89b0dcb052cfda59f7e0f58a2bcfc504
Timóteo <timoteostifft@gmail.com>
2023-04-10 15:28:25 -0300

    feat: add keen-slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,25 @@ import React from 'react'
 import Image from 'next/image';
 import { HomeContainer, Product } from '@/styles/pages/home'
 
+import { useKeenSlider } from 'keen-slider/react';
+
 import shirt1 from '../assets/shirts/1.png';
 import shirt2 from '../assets/shirts/2.png';
-// import shirt3 from '../assets/shirts/3.png';
+import shirt3 from '../assets/shirts/3.png';
+
+import 'keen-slider/keen-slider.min.css';
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2.8,
+      spacing: 48
+    }
+  })
+
   return (
-      <HomeContainer>
-        <Product>
+      <HomeContainer ref={sliderRef} className='keen-slider'>
+        <Product className='keen-slider__slide'>
           <Image src={shirt1} width={520} height={480} alt=""/>
 
           <footer>
@@ -19,7 +30,7 @@ export default function Home() {
           </footer>
         </Product>
 
-        <Product>
+        <Product className='keen-slider__slide'>
           <Image src={shirt2} width={520} height={480} alt=""/>
 
           <footer>
@@ -28,7 +39,23 @@ export default function Home() {
           </footer>
         </Product>
 
-        
+        <Product className='keen-slider__slide'>
+          <Image src={shirt3} width={520} height={480} alt=""/>
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+
+        <Product className='keen-slider__slide'>
+          <Image src={shirt3} width={520} height={480} alt=""/>
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
       </HomeContainer>
     )
 }
